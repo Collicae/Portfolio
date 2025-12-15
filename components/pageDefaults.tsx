867d42001d5bd669f48be9e8706b04f3da9e1460
Caleb Collins <calebcollins@Calebs-MacBook-Air.local>
2025-12-15 15:53:26 -0500

    added additional policy

--- components/pageDefaults.tsx
+++ components/pageDefaults.tsx
@@ -35,7 +35,7 @@ export function FooterDefault() {
                 </div>
 
                 <div className="flex flex-col justify-end items-end">
-                    <h2>This portfolio contains work created by me unless otherwise stated. All third-party content is credited to its respective owners. This site is publicly accessible and does not intentionally contain confidential or sensitive information.</h2>
+                    <h2 className="lg:text-4xl md:text-3xl md:ml-5 text-2xl">This portfolio contains work created by me unless otherwise stated. All third-party content is credited to its respective owners. This site is publicly accessible and does not intentionally contain confidential or sensitive information.</h2>
 
                    <h1 className="lg:text-4xl md:text-3xl md:ml-5 text-2xl">{currentYear} Caleb Collins. All Rights Reserved.</h1>
                     
