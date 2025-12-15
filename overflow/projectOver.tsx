8e595d68fd34f8ae6f7c172bbdeee6e6962b6c6c
Caleb Collins <calebcollins@Calebs-MacBook-Air.local>
2025-12-15 12:18:10 -0500

    added school projects

--- overflow/projectOver.tsx
+++ overflow/projectOver.tsx
@@ -6,6 +6,18 @@ export const completedProjects = [
     {name: "Class Project Real Estate Listings", desc: 
         "One of the school projects I helped make. I was in charge of the front-end of the project (making the layout of the website) though my partner at the time scrapped most of my work I was still able to leave some traces of work here and there, also got me a bit more familiarized with GIT", 
         openDate: "02/20/2025", closeDate: "03/19/2025"},
+
+    {name: "Class Project Starship Enhancment", desc: 
+        "One of the 3 Freshman projects in my first semester, my group members and I were tasked with finding alternative ways Starships could be used around campus. My group came up with the idea that startships could also be used as long distance campus tours robots to help others get a view of a colleges campus without having to travel all the way there. \n\n Group Members: \n\n-Bosco\n-Joey\n-Yanjin\n-Caleb", 
+        openDate: "10/01/2025", closeDate: "10/08/2025"},
+
+    {name: "Class Project Basic App Planning", desc: 
+        "The second of the school projects in my freshman year, I was tasked with creating a mock app that helped people shop better. The app that I mocked up was an app that held all the coupons/deals from different stores to help customers plan better. I also added a map feature in as well so just in case a deal/coupon was in a different store it would show them the quickest route to get there. ", 
+        openDate: "11/01/2025", closeDate: "11/07/2025"},
+
+    {name: "Class Project Basic App Improvements", desc: 
+        "The third of the school projects in my freshman year and last one, from project 2 I was tasked with improving my app. First having to conduct interviews to see what people were having trouble with the app starting out. Then using the feedback from those people use A.I to improve my app in the ways that were lacking in the original version", 
+        openDate: "11/01/2025", closeDate: "12/15/2025"},
 ]
 
 export const completedImage = [
@@ -21,6 +33,21 @@ export const completedImage = [
        '/completed/app/Screenshot 2025-09-30 at 6.28.52 PM.png',
        '/completed/app/Screenshot 2025-09-30 at 6.29.00 PM.png',
     ]},
+    {name: 'Rentable Starships Project', img: [
+       '/completed/school/project1_1.png',
+       '/completed/school/project1_2.png',
+       '/completed/school/project1_3.png',
+     ]},
+    {name: 'Basic App Planning', img: [
+       '/completed/school/project2_Home.png',
+       '/completed/school/project2_Sales.png',
+       '/completed/school/project2_Map.png',
+     ]},
+    {name: 'Basic App Enhancments', img: [
+       '/completed/school/project3_Home.png',
+       '/completed/school/project3_Sales.png',
+       '/completed/school/project3_Map.png',
+     ]},
 ]
 
   
